e8c2f48abc6e52647475f9dfbb62fefd6d26acdc
samroodAli <devsamrood@gmail.com>
2021-03-18 16:44:09 +0530

    refactore functions into two for max method lines limit fix

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,33 +1,46 @@
-#bubble sort for strings
+# frozen_string_literal: true
+
+# bubble sort for strings
 
 def bubble_sort_words(arr)
- sorted = false
-  while !sorted
-    sorted=true
-    (0...arr.length-1).each do |i|
-      if (yield arr[i], arr[i + 1]).to_i >= 0
-        arr[i],arr[i+1] = arr[i+1],arr[i]
-        sorted=false
-      end
+  sorted = false
+  until sorted
+    sorted = loop_words(arr) do |left, right|
+      left.length - right.length
     end
   end
-  return arr
+  arr
 end
-bubble_sort_words(["hi","hello","hello world","a"]) do |left,right|
-    left.length - right.length
+
+def loop_words(arr)
+  is_sorted = true
+  (0...arr.length - 1).each do |i|
+    if (yield arr[i], arr[i + 1]).to_i >= 0
+      arr[i], arr[i + 1] = arr[i + 1], arr[i]
+      is_sorted = false
+    end
+  end
+  is_sorted
 end
 
 # bubble sort for integers
 def bubble_sort(arr)
   sorted = false
-  while !sorted
-    sorted=true
-    (0...arr.length-1).each do |i|
-      if arr[i]> arr[i+1]
-        arr[i],arr[i+1] = arr[i+1],arr[i]
-        sorted=false
-      end
+  sorted = loop_integers(arr) until sorted
+  arr
+end
+
+def loop_integers(arr)
+  is_sorted = true
+  (0...arr.length - 1).each do |i|
+    if arr[i] > arr[i + 1]
+      arr[i], arr[i + 1] = arr[i + 1], arr[i]
+      is_sorted = false
     end
   end
-  return arr
+  is_sorted
 end
+
+# test
+p bubble_sort_words(['hi', 'hello', 'hello world', 'a'])
+p bubble_sort([1, 6, 3, 7, 9])
