70ab542016282e49690f7cf7450acee2deaeccb5
samroodAli <devsamrood@gmail.com>
2021-03-18 20:49:21 +0530

    fix:if no block scenario check, remove helper functions for bubble sort

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,46 +1,40 @@
-# frozen_string_literal: true
-
-# bubble sort for strings
-
-def bubble_sort_words(arr)
-  sorted = false
-  until sorted
-    sorted = loop_words(arr) do |left, right|
-      left.length - right.length
-    end
-  end
-  arr
-end
-
-def loop_words(arr)
-  is_sorted = true
-  (0...arr.length - 1).each do |i|
-    if (yield arr[i], arr[i + 1]).to_i >= 0
-      arr[i], arr[i + 1] = arr[i + 1], arr[i]
-      is_sorted = false
-    end
-  end
-  is_sorted
-end
-
 # bubble sort for integers
 def bubble_sort(arr)
-  sorted = false
-  sorted = loop_integers(arr) until sorted
+  loop do
+    sorted = false
+    (arr.length - 1).times do |i|
+      if arr[i] > arr[i + 1]
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        sorted = true
+      end
+    end
+    break unless sorted
+  end
   arr
 end
 
-def loop_integers(arr)
-  is_sorted = true
-  (0...arr.length - 1).each do |i|
-    if arr[i] > arr[i + 1]
-      arr[i], arr[i + 1] = arr[i + 1], arr[i]
-      is_sorted = false
+# bubble sort for strings
+def bubble_sort_words(arr)
+  if block_given?
+    loop do
+      sorted = false
+      (arr.length - 1).times do |i|
+        if yield(arr[i], arr[i + 1]).positive?
+          arr[i], arr[i + 1] = arr[i + 1], arr[i]
+          sorted = true
+        end
+      end
+      break unless sorted
     end
+    p arr
+    arr
+  else
+    p 'Please give a block when invoking bubble_sort_words function'
   end
-  is_sorted
 end
 
 # test
-p bubble_sort_words(['hi', 'hello', 'hello world', 'a'])
+bubble_sort_words(['hi', 'hello', 'hello world', 'a']) do |left, right|
+  left.length - right.length
+end
 p bubble_sort([1, 6, 3, 7, 9])
