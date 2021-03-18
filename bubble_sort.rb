486dae49ba038b48a4d7dd97f8aa1fdc8a6426ce
Stephanie041996 <michellesakuhuni5@gmail.com>
2021-03-18 12:12:24 +0200

    Add yield bubble sort

---
+++ bubble_sort.rb
@@ -0,0 +1,16 @@
+  sorted = false
+  while !sorted
+    sorted=true
+    (0...arr.length-1).each do |i|
+      if (yield arr[i], arr[i + 1]).to_i >= 0
+        arr[i],arr[i+1] = arr[i+1],arr[i]
+        sorted=false
+      end
+    end
+  end
+  print arr
+  return arr
+end
+bubble_sort_words(["hi","hello","hello world","a"]) do |left,right|
+    left.length - right.length
+end
