e6bb08021731c2a17ba34d5781e6ebf39830c5a7
samroodAli <devsamrood@gmail.com>
2021-03-19 10:51:07 +0530

    Remove Comment for linter fix

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -15,26 +15,19 @@ end
 
 # bubble sort for strings
 def bubble_sort_words(arr)
-  if block_given?
-    loop do
-      sorted = false
-      (arr.length - 1).times do |i|
-        if yield(arr[i], arr[i + 1]).positive?
-          arr[i], arr[i + 1] = arr[i + 1], arr[i]
-          sorted = true
-        end
+  loop do
+    sorted = false
+    (arr.length - 1).times do |i|
+      if yield(arr[i], arr[i + 1]).positive?
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        sorted = true
       end
-      break unless sorted
     end
-    p arr
-    arr
-  else
-    p 'Please give a block when invoking bubble_sort_words function'
+    break unless sorted
   end
+  arr
 end
 
-# test
-bubble_sort_words(['hi', 'hello', 'hello world', 'a']) do |left, right|
-  left.length - right.length
+def bubble_sort_by(arr)
+  bubble_sort_words(arr) { |left, right| left.length <=> right.length }
 end
-p bubble_sort([1, 6, 3, 7, 9])
