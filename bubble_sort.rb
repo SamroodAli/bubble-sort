a57a5a2b8484dd7a7119498f2cb38c2d9844e748
samroodAli <devsamrood@gmail.com>
2021-03-19 10:53:18 +0530

    fix:rubocop linter errors autocorrect in bubble_sort.rb

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -23,31 +23,11 @@ def bubble_sort_words(arr)
         sorted = true
       end
     end
-<<<<<<< HEAD
-<<<<<<< HEAD
     break unless sorted
-=======
-    arr
-  else
-    p 'Please give a block when invoking bubble_sort_words function'
->>>>>>> 9353ca0 (readme:include bubble sort info and image, improve setting up)
   end
   arr
 end
-<<<<<<< HEAD
 
 def bubble_sort_by(arr)
   bubble_sort_words(arr) { |left, right| left.length <=> right.length }
-=======
-    break unless sorted
-  end
-  arr
 end
-
-
-def bubble_sort_by(arr)
-  bubble_sort_words(arr){|left,right| left.length <=> right.length} 
->>>>>>> f59ff25 (fix:modularise bubble sort for words with bubble_sort_by)
-end
-=======
->>>>>>> c4d8966 (build:remove development test code below bubble sort solutions)
